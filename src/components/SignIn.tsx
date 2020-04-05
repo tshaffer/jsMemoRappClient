ad6077b9eb9e6d930eaad8d10b22550c9587b80b
Ted Shaffer <ted@brightsign.biz>
2020-04-05 07:35:28 -0700

    more work on signin form

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,6 +13,33 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
+import { useState, useEffect } from 'react';
+import clsx from 'clsx';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { HashRouter } from 'react-router-dom';
+
+import {
+  loadTags
+} from '../controllers/tags';
+
+import { createHashHistory } from 'history';
+
+import FormControl from '@material-ui/core/FormControl';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+import { validateUser } from '../controllers/user';
+import { setUser } from '../models/user';
+import { User } from '../types';
 
 function Copyright() {
   return (
@@ -46,8 +74,64 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn() {
+interface LoginProps {
+  onLoadTags: () => any;
+  onSetUser: (user: User) => any;
+}
+
+const SignIn = (props: LoginProps) => {
+
   const classes = useStyles();
+
+  const [userNameState, setUserName] = useState('');
+  const [passwordState, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(true);
+
+  const { onLoadTags, onSetUser } = props;
+
+  console.log('pizza69');
+
+  useEffect(() => {
+    onLoadTags();
+  });
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSignIn = (e: any) => {
+
+    console.log(userNameState);
+    console.log(passwordState);
+
+    validateUser(userNameState, passwordState)
+      .then((user: User) => {
+        console.log('validation successful: ', user);
+        onSetUser(user);
+        const hashHistory = createHashHistory();
+        hashHistory.push('/home');
+      }).catch((err: Error) => {
+        alert('Login unsuccessful');
+      });
+  };
+
+  const handleUserNameChange = (e: any) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  const onFormSubmit = (e: any) => {
+    e.preventDefault();
+    handleSignIn(e);
+  };
+
 
   return (
     <Container component='main' maxWidth='xs'>
@@ -59,18 +143,46 @@ export default function SignIn() {
         <Typography component='h1' variant='h5'>
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} noValidate onSubmit={onFormSubmit}>
           <TextField
             variant='outlined'
             margin='normal'
             required
             fullWidth
-            id='email'
-            label='Email Address'
-            name='email'
-            autoComplete='email'
+            id='userName'
+            label='User Name'
+            name='userName'
+            autoComplete='userName'
             autoFocus
+            onChange={handleUserNameChange}
           />
+          <div>
+            <TextField
+              required id='standard-required'
+              label='User name'
+              onChange={handleUserNameChange}
+            />
+          </div>
+          <FormControl>
+            <InputLabel htmlFor='standard-adornment-password'>Password</InputLabel>
+            <OutlinedInput
+              id='standard-adornment-password'
+              type={showPassword ? 'text' : 'password'}
+              value={passwordState}
+              onChange={handlePasswordChange}
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='toggle password visibility'
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
           <TextField
             variant='outlined'
             margin='normal'
@@ -114,4 +226,19 @@ export default function SignIn() {
       </Box>
     </Container>
   );
+};
+
+function mapStateToProps(state: any) {
+  return {
+  };
 }
+
+const mapDispatchToProps = (dispatch: any) => {
+  return bindActionCreators({
+    onLoadTags: loadTags,
+    onValidateUser: validateUser,
+    onSetUser: setUser,
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
