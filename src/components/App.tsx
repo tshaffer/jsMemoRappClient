960a4eb93b09a066f9b027a3d61067ea86f0f7f4
Ted Shaffer <ted@brightsign.biz>
2020-04-05 04:32:53 -0700

    App as classless component, phase 1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,101 +16,110 @@ import { validateUser } from '../controllers/user';
 import { setUser } from '../models/user';
 import { User } from '../types';
 
-export interface AppProps {
-  onLoadTags: () => any;
-  onSetUser: (user: User) => any;
-}
+// export interface AppProps {
+//   onLoadTags: () => any;
+//   onSetUser: (user: User) => any;
+// }
 
 interface AppStateProps {
   userName: string;
   password: string;
 }
 
-class App extends React.Component<AppProps, AppStateProps> {
-
-  constructor(props: any) {
-
-    super(props);
-
-    this.state = {
-      userName: '',
-      password: '',
-    };
-
-    console.log('pizza69');
-
-    this.handleSignIn = this.handleSignIn.bind(this);
-    this.handleUserNameChange = this.handleUserNameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-  }
-
-  componentDidMount() {
-    this.handleLoadTags();
-  }
-
-  handleLoadTags() {
-    this.props.onLoadTags();
-  }
-
-  handleSignIn(e: any) {
-
-    console.log(this.state.userName);
-    console.log(this.state.password);
-
-    validateUser(this.state.userName, this.state.password)
-      .then((user: User) => {
-        console.log('validation successful: ', user);
-        this.props.onSetUser(user);
-        const hashHistory = createHashHistory();
-        hashHistory.push('/home');
-      }).catch((err: Error) => {
-        alert('Login unsuccessful');
-      });
-  }
-
-  handleUserNameChange(e: any) {
-    this.setState({userName: e.target.value});
-  }
-
-  handlePasswordChange(e: any) {
-    this.setState({password: e.target.value});
-  }
-
-  render() {
-    return (
-      <HashRouter>
-        <div>
-          <h2>MemoRapp</h2>
-          <h3>Login</h3>
-          <form noValidate autoComplete="off">
-            <div>
-              <TextField
-                required id="standard-required"
-                label="User name"
-                onChange={this.handleUserNameChange}
-              />
-            </div>
-            <div>
-              <TextField
-                id="standard-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                onChange={this.handlePasswordChange}
-              />
-            </div>
-          </form>
-          <Button
-            variant="contained"
-            onClick={e => this.handleSignIn(e)}
-          >
-            Sign In
-          </Button>
-        </div>
-      </HashRouter>
-    );
-  }
+export interface AppProps {
 }
+
+const App = (props: AppProps) => {
+  return (
+    <div>pizza</div>
+  );
+}
+
+// class App extends React.Component<AppProps, AppStateProps> {
+
+//   constructor(props: any) {
+
+//     super(props);
+
+//     this.state = {
+//       userName: '',
+//       password: '',
+//     };
+
+//     console.log('pizza69');
+
+//     this.handleSignIn = this.handleSignIn.bind(this);
+//     this.handleUserNameChange = this.handleUserNameChange.bind(this);
+//     this.handlePasswordChange = this.handlePasswordChange.bind(this);
+//   }
+
+//   componentDidMount() {
+//     this.handleLoadTags();
+//   }
+
+//   handleLoadTags() {
+//     this.props.onLoadTags();
+//   }
+
+//   handleSignIn(e: any) {
+
+//     console.log(this.state.userName);
+//     console.log(this.state.password);
+
+//     validateUser(this.state.userName, this.state.password)
+//       .then((user: User) => {
+//         console.log('validation successful: ', user);
+//         this.props.onSetUser(user);
+//         const hashHistory = createHashHistory();
+//         hashHistory.push('/home');
+//       }).catch((err: Error) => {
+//         alert('Login unsuccessful');
+//       });
+//   }
+
+//   handleUserNameChange(e: any) {
+//     this.setState({userName: e.target.value});
+//   }
+
+//   handlePasswordChange(e: any) {
+//     this.setState({password: e.target.value});
+//   }
+
+//   render() {
+//     return (
+//       <HashRouter>
+//         <div>
+//           <h2>MemoRapp</h2>
+//           <h3>Login</h3>
+//           <form noValidate autoComplete="off">
+//             <div>
+//               <TextField
+//                 required id="standard-required"
+//                 label="User name"
+//                 onChange={this.handleUserNameChange}
+//               />
+//             </div>
+//             <div>
+//               <TextField
+//                 id="standard-password-input"
+//                 label="Password"
+//                 type="password"
+//                 autoComplete="current-password"
+//                 onChange={this.handlePasswordChange}
+//               />
+//             </div>
+//           </form>
+//           <Button
+//             variant="contained"
+//             onClick={e => this.handleSignIn(e)}
+//           >
+//             Sign In
+//           </Button>
+//         </div>
+//       </HashRouter>
+//     );
+//   }
+// }
 
 function mapStateToProps(state: any) {
   return {
@@ -119,9 +128,9 @@ function mapStateToProps(state: any) {
 
 const mapDispatchToProps = (dispatch: any) => {
   return bindActionCreators({
-    onLoadTags: loadTags,
-    onValidateUser: validateUser,
-    onSetUser: setUser,
+    // onLoadTags: loadTags,
+    // onValidateUser: validateUser,
+    // onSetUser: setUser,
   }, dispatch);
 };
 
