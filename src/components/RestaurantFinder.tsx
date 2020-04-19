7a45e7907e4dfce10a3983dac8418a4a46206496
Ted Shaffer <ted@brightsign.biz>
2020-04-19 06:51:47 -0700

    initial experiments with autocomplete.

--- src/components/RestaurantFinder.tsx
+++ src/components/RestaurantFinder.tsx
@@ -9,8 +9,20 @@ import { createHashHistory } from 'history';
 import { HashRouter } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+const top100Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 },
+  { title: '12 Angry Men', year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+];
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -46,15 +58,38 @@ const RestaurantFinder = (props: RestaurantFinderProps) => {
 
   const classes = useStyles();
 
+  const getTags = () => {
+    return (
+      <div>
+        <Autocomplete
+          multiple
+          id='tags-standard'
+          options={top100Films}
+          getOptionLabel={(option) => option.title}
+          defaultValue={[top100Films[1]]}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant='standard'
+              label='Multiple values'
+              placeholder='Favorites'
+            />
+          )}
+        />
+      </div>
+    );
+  };
+
   return (
     <HashRouter>
       <div>
         <h2>MemoRapp</h2>
         <h3>RestaurantFinder</h3>
+        {getTags()}
       </div>
     </HashRouter>
   );
-}
+};
 
 function mapStateToProps(state: any) {
   return {
